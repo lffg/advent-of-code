60869cdd0c6ef73e2710a5de3356a7818475134d
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2023-12-05 18:07:02 -0300

    Add 03/02

--- 23/day03/main.rs
+++ 23/day03/main.rs
@@ -1,17 +1,10 @@
-use std::{ops::Index, str::FromStr};
+use std::{
+    collections::{HashMap, HashSet},
+    ops::Index,
+    str::FromStr,
+};
 
 static INPUT: &str = include_str!("input.txt");
-
-// 467..114..
-// ...*......
-// ..35..633.
-// ......#...
-// 617*......
-// .....+.58.
-// ..592.....
-// ......755.
-// ...$.*....
-// .664.598..
 
 fn part1(input: &str) -> u32 {
     let grid: Grid = input.parse().expect("valid grid");
@@ -24,7 +17,7 @@ fn part1(input: &str) -> u32 {
         let Some((coord @ (row, _), value)) = iter.next() else {
             break;
         };
-        // The rest of the loop accumulate over digits, so skip non digits.
+        // The rest of the loop accumulates over digits, so skip non digits.
         let Value::Digit(digit) = value else {
             continue;
         };
@@ -63,8 +56,54 @@ fn part1(input: &str) -> u32 {
     sum
 }
 
-fn part2(_input: &str) -> u32 {
-    0
+// Copied from `part1`; changes are highlighted with comments.
+fn part2(input: &str) -> u32 {
+    let grid: Grid = input.parse().expect("valid grid");
+
+    // Keep a track of cogs adjacent to numbers. Keys are cog coord.
+    let mut map = HashMap::<Coord, (/* count */ usize, /* ratio */ u32)>::new();
+
+    let mut iter = grid.entries().peekable();
+    loop {
+        let Some((coord @ (row, _), value)) = iter.next() else {
+            break;
+        };
+        let Value::Digit(digit) = value else {
+            continue;
+        };
+
+        let mut number = *digit;
+
+        // Keep track of all cogs adjacent to the number being accumulated.
+        let mut cogs_seen: HashSet<_> = grid.cog_neighbors(coord).collect();
+
+        loop {
+            let Some((next_coord, Value::Digit(next_digit))) = iter.peek() else {
+                break;
+            };
+            if row != next_coord.0 {
+                break;
+            }
+
+            // Add more adjacent cogs, if needed.
+            cogs_seen.extend(grid.cog_neighbors(*next_coord));
+            number = number * 10 + next_digit;
+
+            iter.next();
+        }
+
+        // Increment number count of cogs in the map.
+        for cog_coord in cogs_seen {
+            let (count, ratio) = map.entry(cog_coord).or_insert((0, 1));
+            *count += 1;
+            *ratio *= number;
+        }
+    }
+
+    // Return the sum of cog ratios with 2 elements.
+    map.into_values()
+        .filter_map(|(count, ratio)| (count == 2).then_some(ratio))
+        .sum()
 }
 
 fn main() {
@@ -77,7 +116,7 @@ type Coord = (usize, usize);
 #[derive(Debug)]
 enum Value {
     Digit(u32),
-    Special,
+    Special(char),
     Empty,
 }
 
@@ -88,7 +127,7 @@ impl TryFrom<char> for Value {
         Ok(match value {
             '.' => Value::Empty,
             c if c.is_ascii_digit() => Value::Digit(c.to_digit(10).unwrap()),
-            _ => Value::Special,
+            c => Value::Special(c),
         })
     }
 }
@@ -141,7 +180,7 @@ impl Grid {
             .map(move |(i, v)| ((i / w, i % w), v))
     }
 
-    fn neighbors(&self, (row, col): Coord) -> impl Iterator<Item = Coord> {
+    fn neighbors(&self, (row, col): Coord) -> impl Iterator<Item = Coord> + '_ {
         static OFFSETS: [(isize, isize); 8] = [
             (-1, -1), // top left
             (-1, 0),  // top center
@@ -152,31 +191,37 @@ impl Grid {
             (1, 0),   // bottom center
             (1, 1),   // bottom right
         ];
-
-        fn bounded_add(bound: usize) -> impl Fn(usize, isize) -> Option<usize> {
-            move |a, b| {
-                let r = a.checked_add_signed(b)?;
-                (r < bound).then_some(r)
-            }
+        fn bounded_add(bound: usize, a: usize, b: isize) -> Option<usize> {
+            let r = a.checked_add_signed(b)?;
+            (r < bound).then_some(r)
         }
-
-        let add_row = bounded_add(self.height);
-        let add_col = bounded_add(self.width);
-
         OFFSETS
             .into_iter()
             .filter_map(move |(row_offset, col_offset)| {
-                Some((add_row(row, row_offset)?, add_col(col, col_offset)?))
+                Some((
+                    bounded_add(self.height, row, row_offset)?,
+                    bounded_add(self.width, col, col_offset)?,
+                ))
             })
     }
 
-    fn special_neighbors(&self, coord: Coord) -> impl Iterator<Item = Coord> + '_ {
-        self.neighbors(coord)
-            .filter(|coord| matches!(self[*coord], Value::Special))
+    fn special_neighbors(&self, coord: Coord) -> impl Iterator<Item = (Coord, char)> + '_ {
+        self.neighbors(coord).filter_map(|coord| {
+            if let Value::Special(char) = self[coord] {
+                Some((coord, char))
+            } else {
+                None
+            }
+        })
     }
 
     fn has_special_neighbors(&self, coord: Coord) -> bool {
         self.special_neighbors(coord).next().is_some()
+    }
+
+    fn cog_neighbors(&self, coord: Coord) -> impl Iterator<Item = Coord> + '_ {
+        self.special_neighbors(coord)
+            .filter_map(|(coord, char)| (char == '*').then_some(coord))
     }
 }
 
@@ -209,17 +254,24 @@ mod tests {
         assert_eq!(
             part2(
                 "\
-Foo
-Bar
-Baz"
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."
             ),
-            000
+            467835
         );
     }
 
     #[test]
     fn test_answers() {
         assert_eq!(part1(INPUT), 546563);
-        assert_eq!(part2(INPUT), 0);
+        assert_eq!(part2(INPUT), 91031374);
     }
 }
